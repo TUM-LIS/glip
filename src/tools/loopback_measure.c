563f1542a7b1c8e95d1af4e918010b4e5e6710d9
Philipp Wagner <philipp.wagner@tum.de>
2015-06-25 11:16:15 +0200

    loopback_measure: Usability improvements
    Fix several small problems with the loopback measurement tool:
    - A new --transfer-size argument can be used to specify the number of
      bytes transferred during the measurement.
    - A progress bar has been added to signal how far the transfer already
      proceeded.
    - The locking was simplified to better serve as example code.

--- src/tools/loopback_measure.c
+++ src/tools/loopback_measure.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014 by the author(s)
+/* Copyright (c) 2014-2015 by the author(s)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,6 +28,7 @@
  *   Stefan Wallentowitz <stefan.wallentowitz@tum.de>
  */
 
+#include <errno.h>
 #include <getopt.h>
 #include <inttypes.h>
 #include <libglip.h>
@@ -38,13 +39,15 @@
 #include <time.h>
 #include <unistd.h>
 
-/* total number of bytes to be transferred during the loopback test */
-static const size_t TRANSFER_SIZE_BYTES = 300*1024*1024; /* 300 MB */
+/* default value for the --transfer-size argument (10 MB) */
+#define TRANSFER_SIZE_DEFAULT (10*1024*1024)
 
 /* write block size in bytes */
 #define WRITE_BLOCK_SIZE 6
 /* read block size in bytes */
 #define READ_BLOCK_SIZE 2048
+/* timeout for blocking reads */
+#define READ_TIMEOUT_MS 100
 
 extern void parse_options(char* str, struct glip_option* options[],
                           size_t *num_options);
@@ -62,6 +65,11 @@ void display_help(void)
            "  the backend to be used. See below for available backends.\n"
            "\n"
            "OPTIONAL ARGUMENTS\n"
+           "-s|--transfer-size SIZE\n"
+           "  number of bytes to be transferred in the test. To get \n"
+           "  meaningful results, choose this parameter large enough to \n"
+           "  transfer data for a couple of seconds at least.\n"
+           "\n"
            "-o|--backend-options\n"
            "  options passed to the backend. Options are key=value pairs \n"
            "  separated by a comma.\n"
@@ -89,10 +97,12 @@ void display_help(void)
 
     free(name);
 
-    printf("\nConsult the documentation for a list of available options for "
-            "each backend.\n");
-    printf("\n");
+    printf("\n"
+           "Consult the documentation for a list of available options for \n"
+           "each backend.\n"
+           "\n");
 }
+
 void display_version(void);
 void display_version(void)
 {
@@ -101,16 +111,22 @@ void display_version(void)
            v->suffix);
 }
 
-size_t current_sent;
-size_t current_received;
-pthread_mutex_t done_mutex;
-pthread_cond_t  done;
+struct glip_ctx *glip_ctx;
+
+size_t transfer_size = TRANSFER_SIZE_DEFAULT;
+
+volatile size_t current_sent;
+volatile size_t current_received;
+volatile int send_done = 0;
 int use_blocking_functions;
 
 pthread_t read_thread;
-pthread_attr_t read_thread_attr;
+pthread_t progressbar_thread;
 
 void* read_from_target(void* ctx_void);
+void exit_measurement(int exit_code);
+void update_progressbar(void);
+void* update_progressbar_thread(void*);
 
 int main(int argc, char *argv[])
 {
@@ -131,11 +147,13 @@ int main(int argc, char *argv[])
             {"nonblock",        no_argument,       0, 'n'},
             {"backend",         required_argument, 0, 'b'},
             {"backend-options", required_argument, 0, 'o'},
+            {"transfer-size",   required_argument, 0, 's'},
             {0, 0, 0, 0}
         };
         int option_index = 0;
 
-        c = getopt_long(argc, argv, "is:vnhb:o:", long_options, &option_index);
+        c = getopt_long(argc, argv, "is:vnhb:o:s:", long_options,
+                        &option_index);
         if (c == -1) {
             break;
         }
@@ -155,6 +173,9 @@ int main(int argc, char *argv[])
             backend_optionstring = optarg;
             parse_options(backend_optionstring, &backend_options,
                           &num_backend_options);
+            break;
+        case 's':
+            transfer_size = strtoul(optarg, NULL, 10);
             break;
         case 'n':
             use_blocking_functions = 0;
@@ -177,40 +198,35 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    pthread_mutex_init(&done_mutex, 0);
-    pthread_cond_init(&done, 0);
+    glip_new(&glip_ctx, backend_name, backend_options, num_backend_options);
 
-    struct glip_ctx *ctx;
-    glip_new(&ctx, backend_name, backend_options, num_backend_options);
-
-    rv = glip_open(ctx, 1);
+    rv = glip_open(glip_ctx, 1);
     if (rv < 0) {
         fprintf(stderr, "ERROR: Cannot open backend\n");
         return -1;
     }
 
-    rv = glip_logic_reset(ctx);
+    rv = glip_logic_reset(glip_ctx);
     if (rv < 0) {
-        fprintf(stderr, "Cannot reset device\n");
+        fprintf(stderr, "ERROR: Cannot reset device\n");
         return -1;
     }
 
     /* create a dumb read thread, just discarding the data */
-    pthread_attr_init(&read_thread_attr);
-    pthread_attr_setdetachstate(&read_thread_attr,
-                                PTHREAD_CREATE_DETACHED);
-    rv = pthread_create(&read_thread, &read_thread_attr,
-                        read_from_target, (void*)ctx);
+    rv = pthread_create(&read_thread, NULL,
+                        read_from_target, (void*)glip_ctx);
+
+    /* use a separate thread to update the progress bar */
+    rv = pthread_create(&progressbar_thread, NULL, update_progressbar_thread,
+                        NULL);
 
     struct timespec start, end;
 
-    pthread_mutex_lock(&done_mutex);
     current_sent = 0;
     current_received = 0;
-    pthread_mutex_unlock(&done_mutex);
 
     printf("Running loopback test with %zu bytes of data. This may take a "
-           "while ...\n", TRANSFER_SIZE_BYTES);
+           "while ...\n", transfer_size);
 
     clock_gettime(CLOCK_MONOTONIC, &start);
 
@@ -218,27 +234,41 @@ int main(int argc, char *argv[])
     for (size_t i = 0; i < WRITE_BLOCK_SIZE; i++) {
         data[i] = i % UINT8_MAX;
     }
-    while (current_sent < TRANSFER_SIZE_BYTES) {
+    while (current_sent < transfer_size) {
         size_t size_written;
         if (use_blocking_functions) {
-            glip_write_b(ctx, 0, WRITE_BLOCK_SIZE, data, &size_written, 0);
+            rv = glip_write_b(glip_ctx, 0, WRITE_BLOCK_SIZE, data,
+                              &size_written, 0);
         } else {
             int sub_idx = current_sent % WRITE_BLOCK_SIZE;
-            glip_write(ctx, 0, WRITE_BLOCK_SIZE - sub_idx, &data[sub_idx], &size_written);
+            rv = glip_write(glip_ctx, 0, WRITE_BLOCK_SIZE - sub_idx,
+                            &data[sub_idx], &size_written);
+        }
+        if (rv != 0 && rv != -ETIMEDOUT) {
+            fprintf(stderr, "Error while writing to GLIP. rv = %d\n", rv);
+            exit_measurement(1);
         }
         current_sent += size_written;
     }
-    if (current_received != current_sent) {
-        pthread_cond_wait(&done, &done_mutex);
-    }
+
+    send_done = 1;
+
+    /* wait for read to finish */
+    pthread_join(read_thread, NULL);
 
     clock_gettime(CLOCK_MONOTONIC, &end);
     double diff = (end.tv_sec * 1.0 + end.tv_nsec / 1000000000.0) -
                   (start.tv_sec * 1.0 + start.tv_nsec / 1000000000.0);
 
-    printf("Sent and received %zu bytes in %.03f seconds = %.03lf MByte/s "
+    /* final update to the progress bar */
+    pthread_cancel(progressbar_thread);
+    pthread_join(progressbar_thread, NULL);
+    update_progressbar();
+    printf("\n\n");
+
+    printf("Sent and received %zu bytes in %.03f seconds = %.01lf kiB/s "
            "(bidirectional)\n",
-           current_sent, diff, (current_sent)/diff/1024/1024);
+           current_sent, diff, (current_sent)/diff/1024);
     printf("Write block size: %u bytes, read block size: %u bytes\n",
            WRITE_BLOCK_SIZE, READ_BLOCK_SIZE);
     if (use_blocking_functions) {
@@ -246,7 +276,7 @@ int main(int argc, char *argv[])
     } else {
         printf("Used non-blocking function calls (glip_read() and glip_write())\n");
     }
-    pthread_mutex_unlock(&done_mutex);
+    glip_close(glip_ctx);
     return 0;
 }
 
@@ -258,13 +288,18 @@ void* read_from_target(void* ctx_void)
     size_t size_read;
     uint8_t data_exp = 0;
 
-    pthread_mutex_lock(&done_mutex);
     int byte = 0;
+    int rv;
     while (1) {
         if (use_blocking_functions) {
-            glip_read_b(ctx, 0, READ_BLOCK_SIZE, data_read, &size_read, 100);
+            rv = glip_read_b(ctx, 0, READ_BLOCK_SIZE, data_read, &size_read,
+                             READ_TIMEOUT_MS);
         } else {
-            glip_read(ctx, 0, READ_BLOCK_SIZE, data_read, &size_read);
+            rv = glip_read(ctx, 0, READ_BLOCK_SIZE, data_read, &size_read);
+        }
+        if (rv != 0 && rv != -ETIMEDOUT) {
+            fprintf(stderr, "Error while reading from GLIP. rv = %d\n", rv);
+            exit_measurement(1);
         }
         current_received += size_read;
 
@@ -273,18 +308,48 @@ void* read_from_target(void* ctx_void)
             if (data_read[i] != data_exp) {
                 fprintf(stderr, "Data verification failed: expected 0x%x, "
                         "got 0x%x at byte %d\n", data_exp, data_read[i], byte);
-                exit(1);
+                exit_measurement(1);
             }
             byte++;
             data_exp = (data_exp + 1) % (UINT8_MAX > WRITE_BLOCK_SIZE ?
                                          WRITE_BLOCK_SIZE : UINT8_MAX);
         }
 
-        if (current_received == current_sent) {
-            pthread_cond_signal(&done);
+        /* done! */
+        if (send_done && current_received >= current_sent) {
+            return NULL;
         }
     }
-    pthread_mutex_unlock(&done_mutex);
 
     return NULL;
+}
+
+void* update_progressbar_thread(void* arg)
+{
+    while (1) {
+        update_progressbar();
+        usleep(200*1000); /* 200 ms update interval */
+    }
+    return NULL;
+}
+
+void update_progressbar()
+{
+    int w = 50; /* progress bar width */
+
+    double ratio  =  current_received/(double)transfer_size;
+    int c = ratio * w;
+
+    printf("% 3.0lf %% [", ratio * 100);
+
+    for (int x=0; x<c; x++) printf("=");
+    for (int x=c; x<w; x++) printf(" ");
+    printf("] % 3.0lf KiB TX/% 3.0lf KiB RX\r", current_sent/1024.0,
+           current_received/1024.0);
+    fflush(stdout);
+}
+
+void exit_measurement(int exit_code)
+{
+    exit(exit_code);
 }
