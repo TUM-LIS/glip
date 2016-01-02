6c2f0d53b3b160ec90f96a6f1069e55fae636395
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-02 23:20:20 +0100

    Rebuild doc after UART added

--- group__backend.js
+++ group__backend.js
@@ -3,6 +3,7 @@ var group__backend =
     [ "Cypress FX2", "group__backend__cypressfx2.html", "group__backend__cypressfx2" ],
     [ "JTAG", "group__backend__jtag.html", "group__backend__jtag" ],
     [ "TCP", "group__backend__tcp.html", "group__backend__tcp" ],
+    [ "UART", "group__backend__uart.html", "group__backend__uart" ],
     [ "glip_option", "structglip__option.html", [
       [ "name", "structglip__option.html#ae46159e64d51a9a157e3aa65a939d433", null ],
       [ "value", "structglip__option.html#ab40ec27cb738a1899e98934c6d7e8cec", null ]
---
+++ group__backend__uart.js
@@ -0,0 +1,5 @@
+var group__backend__uart =
+[
+    [ "UART FPGA Logic", "group__backend__uart-logic.html", null ],
+    [ "UART libglip backend", "group__backend__uart-sw.html", null ]
+];
