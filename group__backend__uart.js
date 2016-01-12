7312f7aadcf49fd9f4b612b0927b591f30856486
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-12 09:50:10 +0100

    Rebuild website

--- group__backend__uart.js
+++ group__backend__uart.js
@@ -1,5 +1,6 @@
 var group__backend__uart =
 [
     [ "UART FPGA Logic", "group__backend__uart-logic.html", "group__backend__uart-logic" ],
+    [ "UART Protocol", "group__backend__uart-protocol.html", null ],
     [ "UART libglip backend", "group__backend__uart-sw.html", null ]
 ];
