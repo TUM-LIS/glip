5b0b7e46820ed7e2b9d1f7167c507cac4d7317bb
Philipp Wagner <philipp.wagner@tum.de>
2017-01-27 14:20:46 +0100

    Fix Nexys4 DDR demo project file

--- src/backend_uart/logic/demo/nexys4ddr/vivado.tcl
+++ src/backend_uart/logic/demo/nexys4ddr/vivado.tcl
@@ -106,6 +106,9 @@ set files [list \
  "[file normalize "$origin_dir/../../../../common/logic/scaler/verilog/glip_downscale.v"]"\
  "[file normalize "$origin_dir/../../../../common/logic/scaler/verilog/glip_upscale.v"]"\
  "[file normalize "$origin_dir/../../../../common/logic/fifo/verilog/fifo_sync_fwft.v"]"\
+ "[file normalize "$origin_dir/../../../../common/logic/fifo/verilog/oh_fifo_sync.v"]"\
+ "[file normalize "$origin_dir/../../../../common/logic/fifo/verilog/oh_memory_dp.v"]"\
+ "[file normalize "$origin_dir/../../../../common/logic/fifo/verilog/oh_memory_ram.v"]"\
  "[file normalize "$origin_dir/../../../../common/logic/cdc/verilog/cdc_wrptr_full.v"]"\
  "[file normalize "$origin_dir/../../../../common/logic/cdc/verilog/cdc_sync_wr2rd.v"]"\
  "[file normalize "$origin_dir/../../../../common/logic/cdc/verilog/cdc_sync_rd2wr.v"]"\
